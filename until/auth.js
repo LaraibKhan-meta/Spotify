61814fcb28a2aad67b1a1c02c7db9fb98a51acfa
Laraibkhan <laraib.khan@metafroliclabs.com>
2025-07-03 18:43:51 +0500

    setup ios, add authorization configurtion

--- until/auth.js
+++ until/auth.js
@@ -1,27 +1,28 @@
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import axios from 'axios';
-import { authorize } from 'react-native-app-auth';
-import { setAccessToken } from '../redux/authToken';
+import {authorize} from 'react-native-app-auth';
+import {setAccessToken} from '../redux/authToken';
 
 const config = {
   clientId: 'fae0936a402146688b9c322f225612a5',
   clientSecret: '253aa54523f24feb9a7c19e569467a2c',
   redirectUrl: 'spotifyapp://callback',
-  scopes: ['user-read-email', 'user-read-private','user-read-recently-played'],
+  scopes: ['user-read-email', 'user-read-private', 'user-read-recently-played'],
   serviceConfiguration: {
     authorizationEndpoint: 'https://accounts.spotify.com/authorize',
     tokenEndpoint: 'https://accounts.spotify.com/api/token',
   },
-   usePKCE: true,
+  usePKCE: true,
 };
 
-export const spotifyLogin = async ({navigation,dispatch}) => {
+export const spotifyLogin = async ({navigation, dispatch}) => {
   try {
-    console.log('Trying to login with Spotify...');
-     const result = await authorize(config);
-        dispatch(setAccessToken(result.accessToken));
-        navigation.navigate('BottomTabs', { screen: 'Home' });
-    console.log('Access Token:', result.accessToken);
+    console.error('Trying to login with Spotify...');
+    const result = await authorize(config);
+    console.error('Access Token:', result.accessToken);
+    dispatch(setAccessToken(result.accessToken));
+    navigation.navigate('BottomTabs', {screen: 'Home'});
+
     return result;
   } catch (error) {
     console.error('Login error', error);
@@ -30,25 +31,32 @@ export const spotifyLogin = async ({navigation,dispatch}) => {
 
 export const fetchRecentlyPlayed = async ({accessToken}) => {
   try {
-    const response = await axios.get('https://api.spotify.com/v1/me/player/recently-played', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
+    const response = await axios.get(
+      'https://api.spotify.com/v1/me/player/recently-played',
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
       },
-    });
+    );
 
-    const track = response.data.items.map((item) => ({
-        id: item.track.id,
-        name: item.track.name,
-        image: item.track.album.images[1]?.url,
-        width: item.track.album.images[1]?.width,
-        height:item.track.album.images[1]?.height
-    }))
+    const track = response.data.items.map(item => ({
+      id: item.track.id,
+      name: item.track.name,
+      image: item.track.album.images[1]?.url,
+      width: item.track.album.images[1]?.width,
+      height: item.track.album.images[1]?.height,
+    }));
 
     console.log('Recently Played Tracks:', response.data);
     return track;
   } catch (error) {
     if (error.response) {
-      console.error('Spotify API error:', error.response.status, error.response.data);
+      console.error(
+        'Spotify API error:',
+        error.response.status,
+        error.response.data,
+      );
     } else {
       console.error('Axios error:', error.message);
     }
